4e1d05eb069a67b607177227e01dd6c3809c771f
paulo <paulo041299@gmail.com>
2020-02-21 14:37:24 -0300

    iniciando MVC

---
+++ config/conexao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace CheckList.config
+{
+    public class Conexao
+    {
+        public static MySqlConnection objCnx = new MySqlConnection();
+
+        public static MySqlCommand objCmd = new MySqlCommand();
+
+        public static MySqlDataReader objFunc;
+
+        public static string conexao = "Server=172.21.0.40;Database=winover;User=root;Pwd=winover;";
+
+    }
+}
--- view/Login.cs
+++ view/Login.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using CheckList.config;
 
 namespace CheckList.view
 {
@@ -20,6 +22,37 @@ namespace CheckList.view
 
         private void Login_Load(object sender, EventArgs e)
         {
+            try
+            {
+                //CONEXAO BD LOGIN/LOGOUT
+                Conexao.objCnx.ConnectionString = Conexao.conexao;
+
+                //ABRE A CONEXAO COM O BANCO
+                Conexao.objCnx.Open();
+            }
+            catch (Exception Erro)
+            {
+                MessageBox.Show("Erro ==> " + Erro.Message, "Erro ao Conectar ao Banco de Dados",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            string strSql = "SELECT nome FROM funcionarios WHERE matricula = 2191";
+
+            //conexão com o comando
+            Conexao.objCmd.Connection = Conexao.objCnx;
+            //Atribui o comando
+            Conexao.objCmd.CommandText = strSql;
+            //Executa a querry
+            Conexao.objFunc = Conexao.objCmd.ExecuteReader();
+
+            if (Conexao.objCnx.State == ConnectionState.Open)
+            {
+                while (Conexao.objFunc.Read())
+                {
+                    Console.WriteLine(Conexao.objFunc[0]);
+                    MessageBox.Show(Conexao.objFunc[0].ToString());
+                }
+            }
             txtNome.Text = Usuarios.Nome;
             txtIdade.Text = Usuarios.Idade.ToString();
         }
