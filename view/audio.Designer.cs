8e2e512d089999aa4dfed3641dc72df7a868b40f
paulo <paulo041299@gmail.com>
2020-02-28 19:15:34 -0300

    audio teste 01

--- view/audio.Designer.cs
+++ view/audio.Designer.cs
@@ -34,6 +34,8 @@
             this.btnUp = new Bunifu.Framework.UI.BunifuThinButton2();
             this.btnBassUp = new Bunifu.Framework.UI.BunifuThinButton2();
             this.btnBassDown = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.teste = new Bunifu.Framework.UI.BunifuThinButton2();
             this.SuspendLayout();
             // 
             // btnMute
@@ -80,7 +82,7 @@
             this.btnDown.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
             this.btnDown.IdleForecolor = System.Drawing.Color.White;
             this.btnDown.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
-            this.btnDown.Location = new System.Drawing.Point(77, 121);
+            this.btnDown.Location = new System.Drawing.Point(76, 199);
             this.btnDown.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnDown.Name = "btnDown";
             this.btnDown.Size = new System.Drawing.Size(250, 50);
@@ -106,7 +108,7 @@
             this.btnUp.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
             this.btnUp.IdleForecolor = System.Drawing.Color.White;
             this.btnUp.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
-            this.btnUp.Location = new System.Drawing.Point(76, 199);
+            this.btnUp.Location = new System.Drawing.Point(76, 121);
             this.btnUp.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnUp.Name = "btnUp";
             this.btnUp.Size = new System.Drawing.Size(250, 50);
@@ -123,7 +125,7 @@
             this.btnBassUp.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
             this.btnBassUp.BackColor = System.Drawing.SystemColors.Control;
             this.btnBassUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBassUp.BackgroundImage")));
-            this.btnBassUp.ButtonText = "BASS UP";
+            this.btnBassUp.ButtonText = "mic mute";
             this.btnBassUp.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnBassUp.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBassUp.ForeColor = System.Drawing.Color.White;
@@ -149,7 +151,7 @@
             this.btnBassDown.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
             this.btnBassDown.BackColor = System.Drawing.SystemColors.Control;
             this.btnBassDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBassDown.BackgroundImage")));
-            this.btnBassDown.ButtonText = "TESTE";
+            this.btnBassDown.ButtonText = "mic down";
             this.btnBassDown.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnBassDown.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBassDown.ForeColor = System.Drawing.Color.White;
@@ -158,7 +160,7 @@
             this.btnBassDown.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
             this.btnBassDown.IdleForecolor = System.Drawing.Color.White;
             this.btnBassDown.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
-            this.btnBassDown.Location = new System.Drawing.Point(406, 121);
+            this.btnBassDown.Location = new System.Drawing.Point(406, 199);
             this.btnBassDown.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnBassDown.Name = "btnBassDown";
             this.btnBassDown.Size = new System.Drawing.Size(250, 50);
@@ -166,11 +168,65 @@
             this.btnBassDown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.btnBassDown.Click += new System.EventHandler(this.BtnBassDown_Click);
             // 
+            // bunifuThinButton21
+            // 
+            this.bunifuThinButton21.ActiveBorderThickness = 1;
+            this.bunifuThinButton21.ActiveCornerRadius = 30;
+            this.bunifuThinButton21.ActiveFillColor = System.Drawing.Color.White;
+            this.bunifuThinButton21.ActiveForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
+            this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
+            this.bunifuThinButton21.BackColor = System.Drawing.SystemColors.Control;
+            this.bunifuThinButton21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuThinButton21.BackgroundImage")));
+            this.bunifuThinButton21.ButtonText = "mic up";
+            this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuThinButton21.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuThinButton21.ForeColor = System.Drawing.Color.White;
+            this.bunifuThinButton21.IdleBorderThickness = 1;
+            this.bunifuThinButton21.IdleCornerRadius = 30;
+            this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
+            this.bunifuThinButton21.IdleForecolor = System.Drawing.Color.White;
+            this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
+            this.bunifuThinButton21.Location = new System.Drawing.Point(406, 121);
+            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.bunifuThinButton21.Name = "bunifuThinButton21";
+            this.bunifuThinButton21.Size = new System.Drawing.Size(250, 50);
+            this.bunifuThinButton21.TabIndex = 32;
+            this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuThinButton21.Click += new System.EventHandler(this.BunifuThinButton21_Click);
+            // 
+            // teste
+            // 
+            this.teste.ActiveBorderThickness = 1;
+            this.teste.ActiveCornerRadius = 30;
+            this.teste.ActiveFillColor = System.Drawing.Color.White;
+            this.teste.ActiveForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
+            this.teste.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
+            this.teste.BackColor = System.Drawing.SystemColors.Control;
+            this.teste.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("teste.BackgroundImage")));
+            this.teste.ButtonText = "teste";
+            this.teste.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.teste.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teste.ForeColor = System.Drawing.Color.White;
+            this.teste.IdleBorderThickness = 1;
+            this.teste.IdleCornerRadius = 30;
+            this.teste.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
+            this.teste.IdleForecolor = System.Drawing.Color.White;
+            this.teste.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(174)))), ((int)(((byte)(180)))));
+            this.teste.Location = new System.Drawing.Point(228, 267);
+            this.teste.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.teste.Name = "teste";
+            this.teste.Size = new System.Drawing.Size(250, 50);
+            this.teste.TabIndex = 33;
+            this.teste.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.teste.Click += new System.EventHandler(this.Teste_Click);
+            // 
             // Audio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(728, 292);
+            this.ClientSize = new System.Drawing.Size(728, 330);
+            this.Controls.Add(this.teste);
+            this.Controls.Add(this.bunifuThinButton21);
             this.Controls.Add(this.btnBassDown);
             this.Controls.Add(this.btnBassUp);
             this.Controls.Add(this.btnUp);
@@ -189,5 +245,7 @@
         private Bunifu.Framework.UI.BunifuThinButton2 btnUp;
         private Bunifu.Framework.UI.BunifuThinButton2 btnBassUp;
         private Bunifu.Framework.UI.BunifuThinButton2 btnBassDown;
+        private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
+        private Bunifu.Framework.UI.BunifuThinButton2 teste;
     }
 }
--- view/audio.cs
+++ view/audio.cs
@@ -9,17 +9,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace CheckList.view
 {
     public partial class Audio : Form
     {
 
-        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+
         private const int WM_APPCOMMAND = 0x319;
-        private const int APPCOMMAND_MICROPHONE_VOLUME_UP = 26 * 65536;
+
+        //MICROFONE 
+        private const int APPCOMMAND_MICROPHONE_VOLUME_MUTE = 24 * 65536;
         private const int APPCOMMAND_MICROPHONE_VOLUME_DOWN = 25 * 65536;
-        private const int APPCOMMAND_MIC_ON_OFF_TOGGLE = 44 * 65536;
-        private const int APPCOMMAND_BASS_BOOST = 44 * 65536;
+        private const int APPCOMMAND_MICROPHONE_VOLUME_UP = 26 * 65536;
+
+        //HEAD
+        private const int APPCOMMAND_VOLUME_MUTE = 8 * 65536;
+        private const int APPCOMMAND_VOLUME_DOWN = 9 * 65536;
+        private const int APPCOMMAND_VOLUME_UP = 10 * 65536;
+
+        //TESTE
+        private const int APPCOMMAND_MEDIA_PLAY_PAUSE = 14 * 65536;
 
         [DllImport("user32.dll")]
         public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, 
@@ -31,29 +41,43 @@ namespace CheckList.view
 
         private void BtnMute_Click(object sender, EventArgs e)
         {
-            //SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
-            //    (IntPtr)APPCOMMAND_VOLUME_MUTE);
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                 (IntPtr)APPCOMMAND_VOLUME_MUTE);//or _DOWN
         }
 
         private void BtnDown_Click(object sender, EventArgs e)
         {
-
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_DOWN);//or _DOWN
         }
 
         private void BtnUp_Click(object sender, EventArgs e)
         {
-            
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_UP);//or _DOWN
         }
 
         private void BtnBassUp_Click(object sender, EventArgs e)
         {
-            
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                 (IntPtr)APPCOMMAND_MICROPHONE_VOLUME_MUTE);//or _DOWN
         }
 
         private void BtnBassDown_Click(object sender, EventArgs e)
         {
             SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
-                 (IntPtr)APPCOMMAND_BASS_BOOST);//or _DOWN
+                (IntPtr)APPCOMMAND_MICROPHONE_VOLUME_DOWN);//or _DOWN
+        }
+
+        private void BunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_MICROPHONE_VOLUME_UP);//or _DOWN
+        }
+
+        private void Teste_Click(object sender, EventArgs e)
+        {
+          
         }
     }
 }
